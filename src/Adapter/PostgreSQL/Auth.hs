3b0b0248d0079f1af8bc8d4c9f1cbd0d9480c04e
decapo <decapo01@gmail.com>
2019-04-10 21:36:32 -0600

    got main pg repo implementation functions compiling

--- src/Adapter/PostgreSQL/Auth.hs
+++ src/Adapter/PostgreSQL/Auth.hs
@@ -55,13 +55,16 @@ withConn action = do
   pool <- asks getter
   liftIO . withResource pool $ \conn -> action conn
 
+genvCode :: Text -> IO Text
+genvCode email = do
+  random <- stringRandomIO "[A-Za-z0-9]{16}"
+  return $ (tshow email) <> "_" <> random
+
 addAuth :: PG r m => D.Auth -> m (Either D.RegistrationError (D.UserId,D.VerificationCode))
 addAuth (D.Auth email pass) = do
   let rawEmail = D.rawEmail email
       rawPassw = D.rawPassword pass
-  vCode <- liftIO $ do
-    r <- stringRandomIO "[A-Za-z0-9]{16}"
-    return $ (tshow rawEmail) <> "_" <> r
+  vCode  <- liftIO $ genvCode rawEmail
   result <- withConn $ \conn ->
     try $ query conn qry (rawEmail,rawPassw,vCode)
   case result of
@@ -78,11 +81,72 @@ addAuth (D.Auth email pass) = do
           \values (?, crypt(?,gen_salt('bf')),?,?) returning id"
 
 
-setEmailAsVerified  :: D.VerificationCode -> m (Either D.EmailVerificationError (D.UserId,D.Email))
-setEmailAsVerified = undefined
+setEmailAsVerified :: PG r m => D.VerificationCode -> m (Either D.EmailVerificationError (D.UserId, D.Email))
+setEmailAsVerified vCode = do
+  result <- withConn $ \conn ->
+    query conn qry (Only vCode)
+  case result of
+    [(uId,email)] -> return $ makeEmail uId email
+    _             -> return $ Left  D.EmailVerificationErrorInvalidCode
+  where
+    qry = "update auths set is_email_verified = true where email_verification_code = ?"
+    makeEmail uId email =
+      case D.mkEmail email of
+        Right e -> Right (uId,e)
+        _       -> Left  D.EmailVerificationErrorInvalidCode
 
-findUserByAuth :: D.Auth -> m (Maybe (D.UserId, Bool))
-findUserByAuth = undefined
+-- my attempt without book...does not compile
+-- setEmailAsVerified  :: PG r m => D.VerificationCode -> m (Either D.EmailVerificationError (D.UserId,D.Email))
+-- setEmailAsVerified vCode = do
+  -- authRes <- liftIO $ findAuthByVcode vCode
+  -- case authRes of
+    -- Left e     -> return $ Left D.EmailVerificationErrorInvalidCode
+    -- Right [Only (uId,email)] -> do
+      -- res2 <- liftIO $ updateAuthAsVerified email
+      -- case res2 of
+        -- Left  e -> return $ Left D.EmailVerificationErrorInvalidCode
+        -- Right _ -> return $ Right (uId,email) 
+  -- where
+    -- findAuthByVcode vCode =
+      -- withConn $ \conn ->
+        -- try $ query conn qry (vCode)
+    -- qry = "select id,email from auths where email_verification_code = ? "
+    -- updateAuthAsVerified email =
+      -- withConn $ \conn ->
+        -- try $ query conn qry2 (email)
+    -- qry2 = "update \
+           -- \  auths \
+           -- \set\
+           -- \  is_email_verified = true \
+           -- \where\
+           -- \  email = ?"
 
-findEmailFromUserId :: D.UserId -> m (Maybe D.Email)
-findEmailFromUserId = undefined
+-- my attempt
+findUserByAuth :: (PG r m) => D.Auth -> m (Maybe (D.UserId, Bool))
+findUserByAuth auth = do
+  res <- withConn $ \conn ->
+    query conn qry (email,passw)
+  return $
+    case res of
+      [(uId,isVerified)] -> Just (uId,isVerified)
+      _                  -> Nothing
+  where
+    qry   = "select id, is_email_verified from auths where email = ? and pass = crypt(?,pass)"
+    email = D.rawEmail    $ D.authEmail    auth
+    passw = D.rawPassword $ D.authPassword auth
+
+
+-- my attempt
+findEmailFromUserId :: (PG r m) => D.UserId -> m (Maybe D.Email)
+findEmailFromUserId uId = do
+  res <- withConn $ \conn ->
+    query conn qry (Only uId)
+  case res of
+    [Only email] -> return $ createEmail email
+    _            -> return $ Nothing
+  where
+    qry = "select cast(email as text) from auths where id = ?"
+    createEmail email =
+      case (D.mkEmail email) of
+        Right e -> Just e
+        _       -> Nothing
